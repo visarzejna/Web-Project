1f9a41b6c87dbe8a6ebcc5fe97b49dfd12483c69
Lorik Tolaj <77443686+loriktolaj@users.noreply.github.com>
2021-02-22 00:07:05 +0100

    Back-end
    Lidhja me serverin, login dhe register funksional.
    edhe disa metoda te rolit admin funksionale.

---
+++ Web-Project/php/controllers/authenticate.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once '../models/AdminModel.php';
+include_once '../models/UserModel.php';
+require_once '../models/User.php';
+require_once "../models/Register.php";
+require_once "../models/Login.php";
+
+session_start();
+//main
+if (isset($_POST['loginBtn'])) {
+    $login = new Login($_POST);
+    $login->login();
+} else if (isset($_POST['registerBtn'])) {
+    $register = new Register($_POST);
+    $register->register();
+} else {
+    header("Location:../../views/Vincero.php");
+}
